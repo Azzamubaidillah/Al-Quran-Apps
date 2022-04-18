bcbbb9ef57227215f6696089c41e32eff7863c7e
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-04-19 02:54:46 +0700

    introduction screen finish

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -5,6 +5,8 @@ import 'package:get/get.dart';
 import '../controllers/home_controller.dart';
 
 class HomeView extends GetView<HomeController> {
+  const HomeView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/app/modules/introduction/views/introduction_view.dart
+++ lib/app/modules/introduction/views/introduction_view.dart
@@ -1,3 +1,4 @@
+import 'package:alquran/app/routes/app_pages.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
@@ -5,17 +6,33 @@ import 'package:get/get.dart';
 import '../controllers/introduction_controller.dart';
 
 class IntroductionView extends GetView<IntroductionController> {
+  const IntroductionView({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('IntroductionView'),
-        centerTitle: true,
-      ),
-      body: Center(
-        child: Text(
-          'IntroductionView is working',
-          style: TextStyle(fontSize: 20),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 16),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Sesibuk itukah kamu,\n sampai belum membaca quran?",
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(height: 15),
+              Image.network(
+                  "https://www.kindpng.com/picc/m/204-2048251_vector-illustration-of-islamic-muslim-koran-or-quran.png"),
+              SizedBox(
+                width: Get.width / 1.8,
+                child: ElevatedButton(
+                  onPressed: () => Get.offAllNamed(Routes.HOME),
+                  child: const Text("Get started"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
