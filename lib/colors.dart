06e47002f144853f337512033746d29e4580a4a0
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-04-21 04:55:33 +0700

    color const

---
+++ lib/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/cupertino.dart';
+
+const mainColor = Color(0xFF672CBC);
+const greyColor = Color(0xFF8789A3);
+const blackColor = Color(0xFF240F4F);
+const orangeColor = Color(0xFFF9B091);
+const darkBackground = Color(0xFF091945);
