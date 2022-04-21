66cd9d090faa57c25972aa239dea3e97eb3df71a
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-04-21 23:12:58 +0700

    pray time ok

--- lib/app/modules/pray_time/views/pray_time_view.dart
+++ lib/app/modules/pray_time/views/pray_time_view.dart
@@ -1,6 +1,7 @@
 import 'package:alquran/app/data/models/response_shalat.dart' as waktu;
 import 'package:alquran/colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -22,77 +23,196 @@ class PrayTimeView extends GetView<PrayTimeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: ListView(
-        children: [
-          Stack(
-            children: [
-              ClipRRect(
-                child: Image.asset(
-                  "assets/illustrations/pray_time.jpg",
-                ),
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(25),
-                  bottomRight: Radius.circular(25),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(marginDefault),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "Pray time",
-                      style: GoogleFonts.poppins(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: white,
+      body: SafeArea(
+        child: ListView(
+          children: [
+            Stack(
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(bottom: marginDefault),
+                  decoration: const BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                        color: Color(
+                          0x19000000,
+                        ),
+                        offset: Offset(
+                          3,
+                          3,
+                        ),
+                        blurRadius: 10,
                       ),
+                    ],
+                  ),
+                  height: Get.height * 0.3,
+                  width: Get.width,
+                  child: ClipRRect(
+                    child: Image.asset(
+                      "assets/illustrations/pray_time.jpg",
+                      fit: BoxFit.cover,
                     ),
-                    StreamBuilder(
+                    borderRadius: const BorderRadius.only(
+                      bottomLeft: Radius.circular(25),
+                      bottomRight: Radius.circular(25),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(marginDefault),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Zona waktu Jakarta",
+                        style: GoogleFonts.poppins(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: blackColor,
+                        ),
+                      ),
+                      StreamBuilder(
                         stream: _clock(),
                         builder: (context, AsyncSnapshot<String> snapshot) {
                           if (snapshot.connectionState ==
                               ConnectionState.waiting) {
-                            return const CircularProgressIndicator();
+                            return Text(
+                              "00:00:00",
+                              style: GoogleFonts.poppins(
+                                fontSize: 44,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            );
                           }
                           return Text(
                             snapshot.data!,
                             style: GoogleFonts.poppins(
                               fontSize: 44,
                               fontWeight: FontWeight.w600,
+                              color: blackColor,
                             ),
                           );
-                        }),
-                  ],
+                        },
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
+            FutureBuilder<waktu.ResponseShalat>(
+                future: controller.getPrayTime(),
+                builder: (context, snapshot) {
+                  waktu.Times? time =
+                      snapshot.data?.results?.datetime?[0].times;
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: Text("Tidak ada data"),
+                    );
+                  }
+                  return Column(
+                    children: [
+                      SholatCard(
+                        time: time,
+                        sholat: "Fajar",
+                        icon: "subuh",
+                        api: "${time?.fajr}",
+                      ),
+                      SholatCard(
+                        time: time,
+                        sholat: "Dzuhur",
+                        icon: "dzuhur",
+                        api: "${time?.dhuhr}",
+                      ),
+                      SholatCard(
+                        time: time,
+                        sholat: "Ashar",
+                        icon: "ashar",
+                        api: "${time?.asr}",
+                      ),
+                      SholatCard(
+                        time: time,
+                        sholat: "Maghrib",
+                        icon: "maghrib",
+                        api: "${time?.maghrib}",
+                      ),
+                      SholatCard(
+                        time: time,
+                        sholat: "Isya",
+                        icon: "isya",
+                        api: "${time?.isha}",
+                      ),
+                    ],
+                  );
+                }),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SholatCard extends StatelessWidget {
+  const SholatCard({
+    Key? key,
+    required this.time,
+    required this.sholat,
+    required this.api,
+    required this.icon,
+  }) : super(key: key);
+
+  final waktu.Times? time;
+  final String sholat;
+  final String api;
+  final String icon;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: Get.width,
+      height: 50,
+      margin: const EdgeInsets.all(marginDefault),
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: marginDefault),
+        child: Row(
+          children: [
+            SvgPicture.asset("assets/icons/$icon.svg"),
+            const SizedBox(width: marginDefault),
+            Text(
+              sholat,
+              style: GoogleFonts.poppins(
+                  fontSize: 14, fontWeight: FontWeight.w500),
+            ),
+            const Expanded(
+              child: SizedBox(),
+            ),
+            Text(api,
+                style: GoogleFonts.poppins(
+                    fontSize: 14, fontWeight: FontWeight.bold)),
+          ],
+        ),
+      ),
+      decoration: BoxDecoration(
+        color: const Color(
+          0xffffffff,
+        ),
+        borderRadius: BorderRadius.circular(12),
+        boxShadow: const [
+          BoxShadow(
+            color: Color(
+              0x19000000,
+            ),
+            offset: Offset(
+              3,
+              3,
+            ),
+            blurRadius: 10,
           ),
         ],
       ),
-    )
-
-        // FutureBuilder<waktu.ResponseShalat>(
-        //     future: controller.getPrayTime(),
-        //     builder: (context, snapshot) {
-        //       waktu.Times? time = snapshot.data?.results?.datetime?[0].times;
-        //       if (snapshot.connectionState == ConnectionState.waiting) {
-        //         return const Center(
-        //           child: CircularProgressIndicator(),
-        //         );
-        //       }
-        //       if (!snapshot.hasData) {
-        //         return const Center(
-        //           child: Text("Tidak ada data"),
-        //         );
-        //       }
-        //       return Column(
-        //         children: [
-
-        //         ],
-        //       );
-        //     }),
-        );
+    );
   }
 }
