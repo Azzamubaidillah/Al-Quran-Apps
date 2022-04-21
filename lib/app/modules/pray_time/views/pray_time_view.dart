90a11c9d8f27e2639db06f93a50abf97c156e894
Azzam Ubaidillah <azzam.ubaidillah05@gmail.com>
2022-04-21 20:50:10 +0700

    clocking hour card

--- lib/app/modules/pray_time/views/pray_time_view.dart
+++ lib/app/modules/pray_time/views/pray_time_view.dart
@@ -1,7 +1,9 @@
 import 'package:alquran/app/data/models/response_shalat.dart' as waktu;
+import 'package:alquran/colors.dart';
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../controllers/pray_time_controller.dart';
 
@@ -20,46 +22,77 @@ class PrayTimeView extends GetView<PrayTimeController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('PrayTimeView'),
-        centerTitle: true,
+        body: SafeArea(
+      child: ListView(
+        children: [
+          Stack(
+            children: [
+              ClipRRect(
+                child: Image.asset(
+                  "assets/illustrations/pray_time.jpg",
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(25),
+                  bottomRight: Radius.circular(25),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(marginDefault),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Pray time",
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: white,
+                      ),
+                    ),
+                    StreamBuilder(
+                        stream: _clock(),
+                        builder: (context, AsyncSnapshot<String> snapshot) {
+                          if (snapshot.connectionState ==
+                              ConnectionState.waiting) {
+                            return const CircularProgressIndicator();
+                          }
+                          return Text(
+                            snapshot.data!,
+                            style: GoogleFonts.poppins(
+                              fontSize: 44,
+                              fontWeight: FontWeight.w600,
+                            ),
+                          );
+                        }),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
-      body: FutureBuilder<waktu.ResponseShalat>(
-          future: controller.getPrayTime(),
-          builder: (context, snapshot) {
-            waktu.Times? time = snapshot.data?.results?.datetime?[0].times;
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(),
-              );
-            }
-            if (!snapshot.hasData) {
-              return const Center(
-                child: Text("Tidak ada data"),
-              );
-            }
-            return Column(
-              children: [
-                StreamBuilder(
-                    stream: _clock(),
-                    builder: (context, AsyncSnapshot<String> snapshot) {
-                      if (snapshot.connectionState == ConnectionState.waiting) {
-                        return const CircularProgressIndicator();
-                      }
-                      return Text(
-                        snapshot.data!,
-                        style:
-                            const TextStyle(fontSize: 50, color: Colors.blue),
-                      );
-                    }),
-                Text("Subuh = ${time?.fajr}"),
-                Text("Dzuhur = ${time?.dhuhr}"),
-                Text("Ashar = ${time?.asr}"),
-                Text("Maghrib = ${time?.maghrib}"),
-                Text("Isya = ${time?.isha}"),
-              ],
-            );
-          }),
-    );
+    )
+
+        // FutureBuilder<waktu.ResponseShalat>(
+        //     future: controller.getPrayTime(),
+        //     builder: (context, snapshot) {
+        //       waktu.Times? time = snapshot.data?.results?.datetime?[0].times;
+        //       if (snapshot.connectionState == ConnectionState.waiting) {
+        //         return const Center(
+        //           child: CircularProgressIndicator(),
+        //         );
+        //       }
+        //       if (!snapshot.hasData) {
+        //         return const Center(
+        //           child: Text("Tidak ada data"),
+        //         );
+        //       }
+        //       return Column(
+        //         children: [
+
+        //         ],
+        //       );
+        //     }),
+        );
   }
 }
